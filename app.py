592c3e22980f17abb71baf3146c187bea44688b1
Hamza Belkadi <137461423+Belkadi-hamza@users.noreply.github.com>
2025-05-31 15:17:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,6 @@ def upload_form():
     }
     
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 4000))
+    app.run(host='0.0.0.0', port=port)
